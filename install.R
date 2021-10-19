1fd17c7ab5ceb8a32ed1767c9abec5839c797f2b
Matt Ingram <40709229+mattcingram@users.noreply.github.com>
2021-10-19 19:00:39 -0400

    Create install.R

---
+++ install.R
@@ -0,0 +1,9 @@
+install.packages(c("pacman", "stargazer", "coefplot", "aod",
+       "ggplot2",
+       "caTools",
+       "rpart",
+       "rpart.plot",
+       "randomForest",
+       "magrittr",
+       "visreg",
+       "rattle"))
